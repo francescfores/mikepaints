612949e5d30bd42253cd0973d1051abfdab9e0e1
Frank <ffores93@gmail.com>
2026-05-22 02:25:45 +0200

    first commit

---
+++ src/app/components/button3d/button3d.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button3d',
+  imports: [],
+  templateUrl: './button3d.html',
+  styleUrl: './button3d.css',
+})
+export class Button3d {}
---
+++ src/app/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button3dComponent} from 'button3d';
+
+@Component({
+  selector: 'app-home',
+  imports: [Button3dComponent],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
